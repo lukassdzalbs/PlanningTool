f7a44021b032c6be96b03d4a26c3218408f807fc
lukassdzalbs <47713560+lukassdzalbs@users.noreply.github.com>
2019-02-22 23:53:17 +0200

    Add files via upload
    init file

---
+++ Client/CalendarWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using CalendarExe1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for CalendarWindow.xaml
+    /// </summary>
+    public partial class CalendarWindow : Window
+    {
+        public CalendarWindow()
+        {
+            InitializeComponent();
+            GoogleIntegrator gi = new GoogleIntegrator();
+            var n = gi.LoadAndParseEvents();
+            this.TxtBlock.Text = n;
+        }
+    }
+}
---
+++ Client/Controls/CustomCalendar.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client.Controls
+{
+    /// <summary>
+    /// Interaction logic for Calendar.xaml
+    /// </summary>
+    public partial class CustomCalendar : UserControl
+    {
+        public CustomCalendar()
+        {
+            InitializeComponent();
+        }
+
+        private DateTime CurrentDateTime;
+
+        private void Set()
+        {
+            int year, month, day;
+            Int32.TryParse(this.YearCtrl.Text, out year);
+            Int32.TryParse(this.MonthCtrl.Text, out month);
+            Int32.TryParse(this.DateCtrl.Text, out day);
+            this.CurrentDateTime = new DateTime(year, month, day);
+        }
+    }
+}
